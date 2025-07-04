ed5da9e917be526c5daa8b75de7084c289bda2f8
CR0N1N-dev <cr0n1ndeveloper@gmail.com>
2025-07-04 12:58:41 +0500

    Add files via upload

---
+++ pages/devblogs/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://www.dropbox.com/scl/fi/aefr9yxt04voudn0zm5wv/devblogs.json?rlkey=3n0ptnn81kh7998om9ivf6i1x&st=9gpdjmms&dl=1")
+        .then(response => response.json())
+        .then(data => renderBlogs(data))
+        .catch(error => console.error("Ошибка при загрузке девблогов:", error));
+});
+
+function renderBlogs(blogList) {
+    const container = document.getElementById("devblog-entries");
+
+    blogList.forEach(blog => {
+        const section = document.createElement("section");
+        section.classList.add("blog-entry");
+
+        section.innerHTML = `
+            <h1>${blog.title}</h1>
+            <div class="container">
+                <p>${blog.content}</p>
+                ${blog.image ? `<img src="${blog.image}" alt="Картинка блога" style="margin-top:20px;max-width:100%;border-radius:10px;">` : ''}
+            </div>
+        `;
+
+        container.appendChild(section);
+    });
+}
+
+const burger = document.getElementById('burger');
+const sideMenu = document.getElementById('sideMenu');
+
+burger.addEventListener('click', () => {
+    sideMenu.classList.toggle('open');
+    burger.classList.toggle('open'); 
+});
