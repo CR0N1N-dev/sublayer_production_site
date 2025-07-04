5ca1085dfeff8f4f405344e964fb9b8ced1b073c
CR0N1N-dev <cr0n1ndeveloper@gmail.com>
2025-07-04 13:06:59 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById('burger');
-const sideMenu = document.getElementById('sideMenu');
-
-burger.addEventListener('click', () => {
-    sideMenu.classList.toggle('open');
-    burger.classList.toggle('open'); 
-});
