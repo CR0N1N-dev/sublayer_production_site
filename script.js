d1edb374f765b72f9a8de5903ecb4c761b2b3122
CR0N1N-dev <cr0n1ndeveloper@gmail.com>
2025-07-04 13:05:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const toggleBtn = document.getElementById("themeToggle");
+const burger = document.getElementById('burger');
+const sideMenu = document.getElementById('sideMenu');
 
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-
-    if (document.body.classList.contains("light-mode")) {
-        toggleBtn.textContent = "Тёмная тема";
-    } else {
-        toggleBtn.textContent = "Светлая тема";
-    }
+burger.addEventListener('click', () => {
+    sideMenu.classList.toggle('open');
+    burger.classList.toggle('open'); 
 });
